4f36fb223dd536573c9c2b59d9bafb2c7285e235
Jon Haddow <haddowjon@protonmail.com>
2020-04-27 20:45:41 +0100

    Improve style and content for 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 
-import { Layout, SEO } from "../components";
+import styles from "./404.module.css";
+
+import { Layout, SEO, NavBar, Header, Title, Card } from "../components";
+import { Link } from "gatsby";
 
 const NotFoundPage: React.FC = () => (
 	<Layout>
 		<SEO title="404: Not found" />
-		<h1>NOT FOUND</h1>
-		<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+		<Header>
+			<NavBar />
+			<Title title="404: Not Found" />
+		</Header>
+		<Card className={styles.card}>
+			<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+			<p>
+				<Link to="/">Go back home</Link> and try again
+			</p>
+		</Card>
 	</Layout>
 );
 
