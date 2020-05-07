2ec8ba6508e41f9c1cecebfc457665b751a1f94c
Jon Haddow <haddowjon@protonmail.com>
2020-05-07 21:08:30 +0100

    Tidy config

--- gatsby-config.js
+++ gatsby-config.js
@@ -162,8 +162,5 @@ module.exports = {
 				pathToConfigModule: `src/utils/typography`,
 			},
 		},
-		// this (optional) plugin enables Progressive Web App + Offline functionality
-		// To learn more, visit: https://gatsby.dev/offline
-		// `gatsby-plugin-offline`,
 	],
 };
