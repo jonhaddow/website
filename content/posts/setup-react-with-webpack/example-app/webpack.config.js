76f5f864a1a38b31b8f096be88aa3e787ed216b0
Jon Haddow <haddowjon@protonmail.com>
2020-10-13 06:12:46 +0100

    Finish write up for webpack react tutorial

--- content/posts/setup-react-with-webpack/example-app/webpack.config.js
+++ content/posts/setup-react-with-webpack/example-app/webpack.config.js
@@ -1,19 +1,22 @@
-var path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	module: {
-		rules: [
-			{
-				test: /\.jsx?/,
-				loader: "babel-loader",
-			},
-		],
-	},
-	plugins: [new HtmlWebpackPlugin()],
-	devServer: {
-		contentBase: path.join(__dirname, "dist"),
-		compress: true,
-		port: 9000,
-	},
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/assets/index.html",
+    }),
+  ],
 };
