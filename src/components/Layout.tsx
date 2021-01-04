1e689b6d3b073de25e828445647efc73dca28711
Jon Haddow <haddowjon@protonmail.com>
2021-01-04 21:40:38 +0000

    Add cloudflare analytics to website

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import "../styles.css";
 
 export const Layout: React.FC = ({ children }) => {
+	// Add analytics to the end of the document
+	useEffect(() => {
+		const script = document.createElement("script");
+
+		script.setAttribute(
+			"src",
+			"https://static.cloudflareinsights.com/beacon.min.js"
+		);
+
+		script.setAttribute(
+			"data-cf-beacon",
+			'{"token": "afd8b37af7cb4ff29b3a5d1a70976ead"}'
+		);
+
+		document.body.appendChild(script);
+	}, []);
+
 	return <>{children}</>;
 };
