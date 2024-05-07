ee067092b945867dec1e6da7ff2562479aae05a2
Jon Haddow <jon@haddow.me>
2024-05-07 20:02:40 +0100

    chore: Remove unused .eslintrc.js file and update npm dependencies

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+const eslintConfigJH = require("eslint-config-jonhaddow");
+const { FlatCompat } = require("@eslint/eslintrc");
+
+const compat = new FlatCompat();
+
+module.exports = [
+  ...eslintConfigJH.base,
+
+  ...eslintConfigJH.react,
+
+  ...compat.extends("plugin:tailwindcss/recommended"),
+
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": "off",
+    },
+  },
+];
