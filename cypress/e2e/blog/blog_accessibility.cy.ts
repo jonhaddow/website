6e9ae0266bca84ce706e0d9765b4419735e05126
Jon Haddow <jon@haddow.me>
2025-09-09 20:49:08 +0100

    Attempt to log violations

--- cypress/e2e/blog/blog_accessibility.cy.ts
+++ cypress/e2e/blog/blog_accessibility.cy.ts
@@ -1,7 +1,29 @@
+// Define at the top of the spec file or just import it
+function terminalLog(violations: any[]) {
+  if (!violations) return;
+  cy.task(
+    "log",
+    `${violations.length} accessibility violation${
+      violations.length === 1 ? "" : "s"
+    } ${violations.length === 1 ? "was" : "were"} detected`,
+  );
+  // pluck specific keys to keep the table readable
+  const violationData = violations.map(
+    ({ id, impact, description, nodes }) => ({
+      id,
+      impact,
+      description,
+      nodes: nodes.length,
+    }),
+  );
+
+  cy.task("table", violationData);
+}
+
 it("Has no detectable a11y violations on load", () => {
   cy.visit("/blog");
   cy.injectAxe();
 
   cy.findByRole("heading", { name: /Blog/ }).should("exist");
-  cy.checkA11y();
+  cy.checkA11y(undefined, undefined, terminalLog);
 });
